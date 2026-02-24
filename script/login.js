e00cdf6255c9b1f81586e2154d41cf34e570b26f
arman115data <mdarmaanmunshi@gmail.com>
2026-02-24 22:33:56 +0600

    work on login.js file and and build a handler function and make if login able and go to the home page

--- script/login.js
+++ script/login.js
@@ -1,1 +1,25 @@
-console.log("login function is create")
+console.log("login function is create");
+
+document.getElementById("login-btn").addEventListener("click", function () {
+  //    1. get the mobile number input
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+  console.log(contactNumber);
+
+  // 2.get the pin input
+  const inputPin = document.getElementById("input-pin");
+  const pin = inputPin.value;
+    console.log(pin);
+    
+    // 3- match pin & mobile number
+    if (contactNumber == "01302700060" && pin == "1234") {
+        alert("login Success");
+
+        // connect to the home.html page
+        window.location.assign("/home.html");
+    }
+    else {
+        alert("login Failed");
+        return;
+    }
+});
