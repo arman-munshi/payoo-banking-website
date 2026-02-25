7d1cdc9bd7d9c16ba756ab047a1b24f1179754c5
arman115data <mdarmaanmunshi@gmail.com>
2026-02-25 21:39:33 +0600

    create new showOnly machine fuction and and add hidden classlist and remove hidden class on the each section and added onclick event on buttons

--- script/machine.js
+++ script/machine.js
@@ -20,3 +20,20 @@ function setBalance(value) {
   const balanceElement = document.getElementById("balance");
   balanceElement.innerText = value;
 }
+
+// machine id > hide all > show id
+
+function showOnly(id) {
+  const addmoney = document.getElementById("add-money");
+  const cashout = document.getElementById("cashout");
+//   console.log(`add Money- ${addmoney} , cashout - ${cashout}`);
+
+  // hide all
+    addmoney.classList.add("hidden");
+    cashout.classList.add("hidden");
+    
+    // id wala element show me
+    const selected = document.getElementById(id);
+    selected.classList.remove("hidden")
+
+}
